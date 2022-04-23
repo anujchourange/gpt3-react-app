7c9f03819e96ef899e0ed6a57c067cb6f81fd278
Anuj Chourange <chourangeanuj@gmail.com>
2022-04-23 13:57:47 +0530

    Navbar done

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import './navbar.css'
+
+// BEM -> Block Element Modifier Naming conevntion is used her for css
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src=''
+        </div>
+
+      </div>
+  )
+}
+
+export default Navbar
